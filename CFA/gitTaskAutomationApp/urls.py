9d52ce9e2b2aaaf11cfb8e0c0175b5a3e9d69421
sandeeppawar32 <pawar93sandeep@gmail.com>
2020-12-27 17:29:17 +0530

    SP: add home page

--- CFA/gitTaskAutomationApp/urls.py
+++ CFA/gitTaskAutomationApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.loginPage, name='login'),
+    path('login/', views.loginPage, name='login'),
     path('home/', views.homePage, name='homePage'),
 ]
