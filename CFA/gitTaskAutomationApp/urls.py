d2f7b5ddac3380f3d7acb3a316e63a463a753f3e
sandeeppawar32 <pawar93sandeep@gmail.com>
2021-02-27 18:07:46 +0530

    (SP) Temp code changes

--- CFA/gitTaskAutomationApp/urls.py
+++ CFA/gitTaskAutomationApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.loginPage, name='login'),
+    path('git/login/', views.loginPage, name='login'),
     path('home/', views.homePage, name='homePage'),
+    path('home/saveDetails', views.homePage, name=''),
 ]
