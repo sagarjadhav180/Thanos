f606321ce51c9806e657654a9e20d20b7626bda5
sagar <sagar.jadhav180@gmail.com>
2021-03-09 16:15:43 +0530

    old project deleted

--- CFA/gitTaskAutomationApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('git/login/', views.loginPage, name='login'),
-    path('home/', views.homePage, name='homePage'),
-    path('home/saveDetails', views.homePage, name=''),
-]
