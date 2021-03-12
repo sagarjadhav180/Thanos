8f744e923ab228e0f41cee5d231e9bb0191ab256
sagar <sagar.jadhav180@gmail.com>
2021-03-12 13:49:19 +0530

    TDC code

--- CFA_API/src/com/convirza/cfa_testdata/CFANumber.java
+++ CFA_API/src/com/convirza/cfa_testdata/CFANumber.java
@@ -1,17 +1,28 @@
 package com.convirza.cfa_testdata;
 
+import com.convirza.tests.base.TestDataPreparation;
+import com.convirza.tests.core.utils.DBNumberUtil;
+
 public class CFANumber implements Number {
 
 	@Override
 	public void createReserveNumber() {
 		// TODO Auto-generated method stub
-		
+		TestDataPreparation testDataPreparation = new TestDataPreparation();
+		String[] numberDetails = testDataPreparation.getNumberFromAPI();
+		testDataPreparation.reserveNumberFromAPI(Long.parseLong(numberDetails[0]), Long.parseLong(numberDetails[1]));
 	}
 
 	@Override
-	public void createPremiumNumber() {
+	public void createPremiumNumber(String orgUnitID) {
 		// TODO Auto-generated method stub
+		//reserving number
+		TestDataPreparation testDataPreparation = new TestDataPreparation();
+		String[] numberDetails = testDataPreparation.getNumberFromAPI();	
+		testDataPreparation.reserveNumberFromAPI(Long.parseLong(numberDetails[0]), Long.parseLong(numberDetails[1]));
 		
+		//making number premium
+		DBNumberUtil.makePremiumNumber(orgUnitID, numberDetails[0]);
 	}
 
 }
