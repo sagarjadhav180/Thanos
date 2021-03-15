f1af12402c908895f354afffe31611af9ca8b801
sandeeppawar32 <pawar93sandeep@gmail.com>
2021-03-15 20:25:32 +0000

    Trigger Build

--- CFA/mysite/mysite/userdetail/views.py
+++ CFA/mysite/mysite/userdetail/views.py
@@ -34,7 +34,8 @@ def index(request):
         p_number = request.POST.get('premium')
         r_number = request.POST.get('reserve')
         stage = request.POST.get('stageENV')
-        components = ['f', 'k']
+        components = request.POST.get('componentId')
+        print(components, "z@@@@@@@@@@@@@@@")
         saveuserdetails1 = saveuserdata(groups=groups, campaign=campaign, t_number=t_number, calls=calls, Tags=Tags, Webhooks=Webhooks, p_number=p_number, r_number=r_number, stage=stage, components=components);
         saveuserdetails1.save();
         gitSave(request);
